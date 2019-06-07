9d77571049f403f237c91127bda9430b7a82dfa4
Morosov Ivan <w-e-r-n-e-r@bk.ru>
2019-06-07 14:20:13 +0500

    updated: formula

--- mintgen.rb
+++ mintgen.rb
@@ -1,9 +1,9 @@
 class Mintgen < Formula
   desc "Code generator for Xcode"
   homepage "https://github.com/mintrocket/mintgen"
-  url "https://github.com/mintrocket/mintgen/releases/download/release_0.1/mintgen-0.1.tar.gz"
-  sha256 "38fc727777aa32df7138af81859dd9bf8257765ef16931fbe5a17a1fa1aa3726"
-  version "0.1"
+  url "https://github.com/mintrocket/mintgen/releases/download/release_0.2/mintgen-0.2.tar.gz"
+  sha256 "269e47da24ad8572ce841ddc6d6983f2534303f96a412878b2ba39bc725fa605"
+  version "0.2"
 
   def install
     bin.install "mintgen"
